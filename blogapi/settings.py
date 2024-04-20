fc94ed7b991d9706ae08628f4d031f9ef27e79c0
marvsconcept <marvade8@gmail.com>
2024-04-21 00:36:03 +0100

    Implementing token authentication

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # 3rd-party apps
     "rest_framework",
     "corsheaders",
+    "rest_framework.authtoken", 
     # Local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
@@ -51,7 +52,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
 
