606d03d655cd8d641b5883778fea698fa8b17286
marvsconcept <marvade8@gmail.com>
2024-04-30 22:22:37 +0100

    Changed Database to Postgres

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -121,12 +121,25 @@ WSGI_APPLICATION = 'blogapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'LDJzmuDHkTclkytOwYqAXbjRkAtpmTKC',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '52846',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
